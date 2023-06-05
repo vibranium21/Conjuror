08fddbabc11a7c9d06161b2c4916aa5193b63ccc
vibranium21 <84234931+vibranium21@users.noreply.github.com>
2023-06-05 15:42:46 -0400

    Update movegeneration.py

--- movegeneration.py
+++ movegeneration.py
@@ -3,5 +3,5 @@ from evaluation import evaluate
 
 def minimax(depth, board, maximizing_player, minimizing_color ):
     if depth == 0 or board.gameover:
-        return None, evaluate()
+        return None, evaluate(board)
     
