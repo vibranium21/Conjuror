053f96784a1d18b775359d255478715b9d2c11d6
vibranium21 <84234931+vibranium21@users.noreply.github.com>
2023-06-05 15:27:24 -0400

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,5 @@
 import chess
 
-
 piece_values = {
     chess.PAWN: 150,
     chess.ROOK: 490,
@@ -10,17 +9,20 @@ piece_values = {
     chess.KING: 20000
 }
 
-
-board = chess.Board(chess.STARTING_FEN)
-white_material = 0
-black_material = 0
-def evaluate():
+def evaluate(fen):
+    board = chess.Board(fen)
+    eval = 0
     for square in chess.SQUARES:
         piece = board.piece_at(square)
         if not piece:
             continue
-            if piece.color == chess.WHITE:
-                eval += piece_values[piece.piece_type]
-            else:
-                eval -= piece_values[piece.piece_type]
+        if piece.color == chess.WHITE:
+            eval += piece_values[piece.piece_type]
+        else:
+            eval -= piece_values[piece.piece_type]
     return eval
+
+
+
+
+
