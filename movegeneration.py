c8d1488e82fb8463c9ae6e35c142aa74f6d57954
vibranium21 <84234931+vibranium21@users.noreply.github.com>
2023-06-05 17:21:25 -0400

    Update movegeneration.py

--- movegeneration.py
+++ movegeneration.py
@@ -1,7 +1,32 @@
 from evaluation import evaluate
+import random
+import chess
+
+def Minimax_Get_Move(game_state, depth, player_color):
+    
+    if depth == 0 or game_state.is_game_over():
+        return None, evaluate(game_state)
+
+    
+    legal_moves = list(game_state.legal_moves)
+    best_move = None
+    best_evaluation = float('-inf') if player_color == chess.WHITE else float('inf')
+    for move in legal_moves:
+        new_game_state = game_state.copy()
+        new_game_state.push(move)
+
+        
+        _, evaluation = Minimax_Get_Move(new_game_state, depth - 1, player_color)
 
 
-def minimax(depth, board, maximizing_player, minimizing_color ):
-    if depth == 0 or board.gameover:
-        return None, evaluate(board)
+        if player_color == chess.WHITE:
+            if evaluation > best_evaluation:
+                best_evaluation = evaluation
+                best_move = move
+        else:
+            if evaluation < best_evaluation:
+                best_evaluation = evaluation
+                best_move = move
+
+    return best_move, best_evaluation
     
