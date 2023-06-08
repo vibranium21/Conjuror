07613feee043665878fc2fa2d854b80f7fbee5e2
vibranium21 <84234931+vibranium21@users.noreply.github.com>
2023-06-08 14:02:38 -0400

    Betterify testingboard.py

--- testingboard.py
+++ testingboard.py
@@ -3,22 +3,20 @@ import chess.svg
 from movegeneration import Minimax_Get_Move
 import math
 
-def print_board(board):
-    print(board)
+
 
 def play_game():
     board = chess.Board()
-    print_board(board)
-
     while not board.is_game_over():
         if board.turn == chess.WHITE:
-            move = Minimax_Get_Move(board, 5, chess.WHITE, -2000000, math.inf)
+            move, _ = Minimax_Get_Move(board, 3, chess.WHITE, -2000000, math.inf)
+            board.push(move)
         else:
-            # Get the move from the other player or external engine
-            move_str = input("Enter your move: ")
-            move = chess.Move.from_uci(move_str)
-
-        board.push(move)
-        print_board(board)
+            move = input("Enter your move: ")
+            board.push_san(move)
+        print(board)
 
 play_game()
+
+
+
