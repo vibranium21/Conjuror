163bd08ddbd00c8e8dce4c0a7c75b4f37d658589
vibranium21 <84234931+vibranium21@users.noreply.github.com>
2023-06-08 10:29:23 -0400

    Update testingboard.py

--- testingboard.py
+++ testingboard.py
@@ -1,7 +1,7 @@
 import chess
 import chess.svg
-import math
 from movegeneration import Minimax_Get_Move
+import math
 
 def print_board(board):
     print(board)
@@ -10,11 +10,15 @@ def play_game():
     board = chess.Board()
     print_board(board)
 
-    while not board.is_game_over():  
-        move = Minimax_Get_Move(board, 5, chess.WHITE, -20000000, math.inf)
+    while not board.is_game_over():
+        if board.turn == chess.WHITE:
+            move = Minimax_Get_Move(board, 5, chess.WHITE, -2000000, math.inf)
+        else:
+            # Get the move from the other player or external engine
+            move_str = input("Enter your move: ")
+            move = chess.Move.from_uci(move_str)
+
         board.push(move)
-
         print_board(board)
-
 
 play_game()
