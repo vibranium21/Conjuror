3a14acb525910b704d32a3fa1c8e6b8912ccf9d4
vibranium21 <84234931+vibranium21@users.noreply.github.com>
2023-06-10 10:37:34 -0400

    Make more versataile -  testingboard.py

--- testingboard.py
+++ testingboard.py
@@ -1,22 +1,38 @@
 import chess
-import chess.svg
-from movegeneration import Minimax_Get_Move
 import math
-
-
+from movegeneration import Minimax_Get_Move
 
 def play_game():
     board = chess.Board()
+
     while not board.is_game_over():
-        if board.turn == chess.WHITE:
-            move, _ = Minimax_Get_Move(board, 3, chess.WHITE, -2000000, math.inf)
+        move, _ = Minimax_Get_Move(board, 4, chess.WHITE, -math.inf, math.inf)
+        
+        if move is not None:
             board.push(move)
-        else:
-            move = input("Enter your move: ")
-            board.push_san(move)
+        
         print(board)
+        print("\n-----------------\n")
+
+        if board.is_game_over():
+            break
+        
+        valid_move = False
+        while not valid_move:
+            try:
+                user_move = input("Enter your move (in UCI notation): ")
+                move = chess.Move.from_uci(user_move)
+                if move in board.legal_moves:
+                    valid_move = True
+                    board.push(move)
+                else:
+                    print("Invalid move. Try again.")
+            except:
+                print("Invalid move format. Try again.")
+
+        print(board)
+        print("\n-----------------\n")
 
 play_game()
-
 
 
